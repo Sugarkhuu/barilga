607f5973ab971282b6540845a9b7b5af1ccbd042
py4econ <py4econ@gmail.com>
2024-10-25 23:33:27 +0200

    remove dt

--- code/prep_data.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-datadir = 'data/'
-# Load the data 
-df = pd.read_csv(datadir + 'data_uneguiplot.csv')
-df = df[df['date'] > '2024-04-01']
-
-
-df.groupby('date')['price_m2'].agg(['median','mean', 'count']).to_csv(datadir + 'data_uneguiplot_dailyprice.csv')
